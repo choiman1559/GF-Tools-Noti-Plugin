7924bf93fddebb5a9f2e4d4224cee00581f235e4
choiman1559 <cuj1559@gmail.com>
2020-08-27 22:58:40 +0900

    updated deprecated google apis

--- app/src/main/java/com/fqxd/gftools/noti/plugin/SettingActivity.java
+++ app/src/main/java/com/fqxd/gftools/noti/plugin/SettingActivity.java
@@ -11,36 +11,33 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.Set;
 
-public class SettingActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class SettingActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 100;
     private FirebaseAuth mAuth;
-    private GoogleApiClient mGoogleApiClient;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,42 +53,37 @@ public class SettingActivity extends AppCompatActivity implements GoogleApiClien
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
         mAuth = FirebaseAuth.getInstance();
 
         final SharedPreferences prefs = getSharedPreferences("Prefs", MODE_PRIVATE);
 
-        final Switch onoff = findViewById(R.id.Enable);
+        final SwitchCompat onoff = findViewById(R.id.Enable);
         final Button glogin = findViewById(R.id.glogin);
         final TextView guid = findViewById(R.id.guid);
 
         if (!prefs.getString("uid", "").equals(""))
-            guid.setText("User Id : " + prefs.getString("uid", ""));
-        else guid.setText(R.string.UserId);
+            guid.setText("Logined as " + mAuth.getCurrentUser().getEmail());
+        else guid.setVisibility(View.GONE);
 
         onoff.setChecked(prefs.getBoolean("Enabled", false));
-        onoff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(onoff.isChecked() && Build.VERSION.SDK_INT > 28 && !Settings.canDrawOverlays(SettingActivity.this)) {
-                    Toast.makeText(SettingActivity.this, "이 기능을 사용하기 위해 다른 앱 위에 그리기 권한이 필요합니다!", Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                    onoff.setChecked(false);
+        onoff.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(onoff.isChecked() && Build.VERSION.SDK_INT > 28 && !Settings.canDrawOverlays(SettingActivity.this)) {
+                Toast.makeText(SettingActivity.this, "이 기능을 사용하기 위해 다른 앱 위에 그리기 권한이 필요합니다!", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                onoff.setChecked(false);
+            } else {
+                Set<String> sets = NotificationManagerCompat.getEnabledListenerPackages(SettingActivity.this);
+                if (sets.contains(getPackageName())) {
+                    prefs.edit().putBoolean("Enabled", onoff.isChecked()).apply();
+                    if (!getSharedPreferences("Prefs", MODE_PRIVATE).getString("uid", "").equals(""))
+                        FirebaseMessaging.getInstance().subscribeToTopic(getSharedPreferences("Prefs", MODE_PRIVATE).getString("uid", ""));
                 } else {
-                    Set<String> sets = NotificationManagerCompat.getEnabledListenerPackages(SettingActivity.this);
-                    if (sets.contains(getPackageName())) {
-                        prefs.edit().putBoolean("Enabled", onoff.isChecked()).apply();
-                        if (!getSharedPreferences("Prefs", MODE_PRIVATE).getString("uid", "").equals(""))
-                            FirebaseMessaging.getInstance().subscribeToTopic(getSharedPreferences("Prefs", MODE_PRIVATE).getString("uid", ""));
-                    } else {
-                        Toast.makeText(SettingActivity.this, "이 기능을 사용하기 위해 알람 엑세스 권한이 필요합니다!", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
-                        prefs.edit().putBoolean("Enabled", false).apply();
-                        onoff.setChecked(false);
-                    }
+                    Toast.makeText(SettingActivity.this, "이 기능을 사용하기 위해 알람 엑세스 권한이 필요합니다!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+                    prefs.edit().putBoolean("Enabled", false).apply();
+                    onoff.setChecked(false);
                 }
             }
         });
@@ -106,25 +98,22 @@ public class SettingActivity extends AppCompatActivity implements GoogleApiClien
             onoff.setEnabled(false);
             prefs.edit().putBoolean("Enabled", false).apply();
         }
-        glogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-                assert cm.getActiveNetworkInfo() != null;
-                boolean isOnline = cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+        glogin.setOnClickListener(v -> {
+            ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            assert cm.getActiveNetworkInfo() != null;
+            boolean isOnline = cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
 
-                if (prefs.getString("uid", "").equals("") && isOnline) {
-                    Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-                    startActivityForResult(signInIntent, RC_SIGN_IN);
-                } else if(isOnline) signOut(onoff,prefs);
-                else Toast.makeText(SettingActivity.this,"Check internet and try again!",Toast.LENGTH_LONG).show();
-            }
+            if (prefs.getString("uid", "").equals("") && isOnline) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            } else if(isOnline) signOut(onoff,prefs);
+            else Toast.makeText(SettingActivity.this,"Check internet and try again!",Toast.LENGTH_LONG).show();
         });
     }
 
-    public void signOut(Switch onoff,SharedPreferences prefs) {
+    public void signOut(SwitchCompat onoff, SharedPreferences prefs) {
         mAuth.signOut();
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+        mGoogleSignInClient.signOut();
         SettingActivity.this.recreate();
         prefs.edit().remove("uid").apply();
 
@@ -148,24 +137,15 @@ public class SettingActivity extends AppCompatActivity implements GoogleApiClien
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(SettingActivity.this, "인증 실패", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(SettingActivity.this, "구글 로그인 인증 성공", Toast.LENGTH_SHORT).show();
-                            final TextView guid = findViewById(R.id.guid);
-                            guid.setText("User Id : " + mAuth.getUid());
-                            getSharedPreferences("Prefs", MODE_PRIVATE).edit().putString("uid", mAuth.getUid()).apply();
-                            SettingActivity.this.recreate();
-                        }
+                .addOnCompleteListener(this, task -> {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(SettingActivity.this, "인증 실패", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SettingActivity.this, "구글 로그인 인증 성공", Toast.LENGTH_SHORT).show();
+                        getSharedPreferences("Prefs", MODE_PRIVATE).edit().putString("uid", mAuth.getUid()).apply();
+                        SettingActivity.this.recreate();
                     }
                 });
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
     }
 
     private boolean isPackageInstalled(String packageName, PackageManager packageManager) {
